54fccb856e08f9fc69d3c257bc30f1d2dbb2fffc
anu1820 <anurajpatil35@gmail.com>
2022-12-30 17:14:25 +0530

    Starting the Game

---
+++ SnakeLadder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeladderGame
+{
+    public class SnakeLadder
+    {
+        public static void Startgame()
+        {
+            int position = 0;
+            int Player1;
+
+            Player1 = position;
+            Console.WriteLine("Player 1 Position is (Player 1)");
+        }
+    }
+}
